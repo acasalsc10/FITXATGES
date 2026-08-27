e055ed1b326f213d27effbbc8ce0965743cd8293
acasalsc10 <acasals26@gmail.com>
2026-08-27 09:39:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Service worker: desa l'app en caché perquè funcioni sense connexió un cop instal·lada.
+var CACHE_NAME = "fitxatges-cache-v1";
+var FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(FILES); })
+  );
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).then(function(resp){
+        var copy = resp.clone();
+        caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+        return resp;
+      }).catch(function(){ return cached; });
+    })
+  );
+});
